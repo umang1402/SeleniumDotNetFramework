1fb52d661c6e88da648d2b649365503422487b00
umang1402 <49523915+umang1402@users.noreply.github.com>
2021-05-27 13:32:04 +0200

    Updated

--- ZAPScan.sh
+++ ZAPScan.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+chmod +x
+
 docker run --rm -v /zap/wrk/:rw -t owasp/zap2docker-stable zap-full-scan.py -t https://ptmserver-production.omicron.at/ -g gen.conf -x OWASP-ZAP-Report.xml -r scan-report.html
