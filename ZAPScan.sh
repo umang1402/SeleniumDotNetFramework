fdaab36937ce6ed7f10b4e8759fa900bcf51a518
umang1402 <49523915+umang1402@users.noreply.github.com>
2021-05-27 13:25:39 +0200

    Update ZAP file

--- ZAPScan.sh
+++ ZAPScan.sh
@@ -1,5 +1,1 @@
-chmod -R 777  ./
-
-docker run --rm -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-full-scan.py -t https://ml-sfra.azurewebsites.net/Analyze/Noise?id=  -g gen.conf -x OWASP-ZAP-Report.xml -r scan-report.html
-
-true
+docker run --rm -v /zap/wrk/:rw -t owasp/zap2docker-stable zap-full-scan.py -t https://ptmserver-production.omicron.at/ -g gen.conf -x OWASP-ZAP-Report.xml -r scan-report.html
